ddb77022ca3f92dec53f14d3f857e3213e162b51
JOJO840 <josip840@gmail.com>
2023-05-12 09:18:49 +0200

    added changes in tekton bash script

--- tekton-cmds.sh
+++ tekton-cmds.sh
@@ -3,12 +3,9 @@
 # 1. update the apt manager
 sudo apt-get update
 
-# 2. Grant permission to Rbac, Using RBAC Authorization
-kubectl apply -f ./rbac-permission.yaml
-
-# 3. Installing tekton pipelines and CRDs
+# 2. Installing tekton pipelines and CRDs
 kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 
-# 4. Installing Tekton Dashboard and CRDs
+# 3. Installing Tekton Dashboard and CRDs
 kubectl apply -f https://storage.googleapis.com/tekton-releases/dashboard/latest/release.yaml
 
